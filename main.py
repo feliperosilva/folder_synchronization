c317e48358d357e78d247a99f25a0aaea6baf5a7
Felipe Silva <frsilva.dev@gmail.com>
2025-02-15 19:21:56 +0000

    Log newly created folders and remove empty directories

--- main.py
+++ main.py
@@ -44,22 +44,29 @@ def update_replica(source_folder, replica_folder):
     # Check if there are files in replica folder that is not in the source folder
     files_to_delete = replica_folder_files - source_folder_files
 
+    subfolder_added = []
     files_added = []
     files_removed = []
     files_updated = []
+    subfolder_removed = []
 
-    # Add files from source folder that do not exist in replica folder
+
+    # Add folders and files from source folder that do not exist in replica folder
     for file_path in files_to_copy:
         src_path = os.path.join(source_folder, file_path)
         dest_path = os.path.join(replica_folder, file_path)
-        os.makedirs(os.path.dirname(dest_path), exist_ok=True)  # Ensure directories exist
+        dest_path_parent = os.path.dirname(dest_path)
+        if not os.path.exists(dest_path_parent): # Check for subfolders
+            os.makedirs(dest_path_parent) # Create them if they do not exist
+            subfolder_added.append(dest_path_parent)
         shutil.copy2(src_path, dest_path) # Copy and preserve timestamps
-        files_added.append(file_path)
+        files_added.append(os.path.join(source_folder, file_path))
     
     # Deleting files from replica folder that do not exist in source folder
     for file_path in files_to_delete:
+        files_removed.append(os.path.join(replica_folder, file_path))
         os.remove(os.path.join(replica_folder, file_path))
-        files_removed.append(os.path.basename(file_path))
+        
 
     # Check if there was any change in a file in source folder that already exists in replica folder using timestamps
     # If there was a change, update the file in replica folder
@@ -71,21 +78,31 @@ def update_replica(source_folder, replica_folder):
                 shutil.copy2(src_path, dest_path)
                 files_updated.append(file_path)
 
+    for dirpath, dirnames, _ in os.walk(replica_folder, topdown=False): # Check subfolder before their parent
+        for dirname in dirnames:
+            if os.listdir(os.path.join(dirpath, dirname)) == []:
+                subfolder_removed.append(os.path.join(dirpath, dirname))
+                os.rmdir(os.path.join(dirpath, dirname))
 
-    return files_added, files_removed, files_updated
+
+    return subfolder_added, files_added, files_removed, files_updated, subfolder_removed
 
 # Create logs in logs/sync.log of each file added or removed to replica folder
-def log_changes(files_added, files_removed, files_updated, log_file='logs/sync.log'):
+def log_changes(subfolder_added, files_added, files_removed, files_updated, subfolder_removed, log_file='logs/sync.log'):
 
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     log_entry = f'[{timestamp}] Changes:\n'
 
+    if subfolder_added:
+        log_entry += f'New folder(s) added: {", ".join(subfolder_added)}\n'
     if files_added:
-        log_entry += f'Files added: {", ".join(files_added)}\n'
+        log_entry += f'File(s) added: {", ".join(files_added)}\n'
     if files_removed:
-        log_entry += f'Files removed: {", ".join(files_removed)}\n'
+        log_entry += f'File(s) removed: {", ".join(files_removed)}\n'
     if files_updated:
-        log_entry += f'Files updated: {", ".join(files_updated)}\n'
+        log_entry += f'File(s) updated: {", ".join(files_updated)}\n'
+    if subfolder_removed:
+        log_entry += f'Empty folder(s) removed: {", ".join(subfolder_removed)}\n'
 
     # Log the changes when they happen
     if files_added or files_removed or files_updated:
@@ -97,8 +114,8 @@ def log_changes(files_added, files_removed, files_updated, log_file='logs/sync.l
 def start_sync(source, replica, sync_interval):
     try:
         while True:
-            files_added, files_removed, files_updated = update_replica(source, replica)
-            log_changes(files_added, files_removed, files_updated)
+            subfolder_added, files_added, files_removed, files_updated, subfolder_removed = update_replica(source, replica)
+            log_changes(subfolder_added, files_added, files_removed, files_updated, subfolder_removed)
             time.sleep(sync_interval)
     except KeyboardInterrupt:
         print('Sync terminated.')
